0d9f1b9a056de7c3e5dc39cd26a0a00eb41b0ec3
Guamusti <marcos.munoz.sevillano@gmail.com>
2026-08-27 19:10:25 +0200

    feat: link community map to spending comparator

--- tests/test_api.py
+++ tests/test_api.py
@@ -46,6 +46,7 @@ def test_index_exposes_share_metadata_in_spanish():
     assert "function PageMetadata" in app
     assert "function ShareMetadata" in app
     assert "function ContractEventTimeline" in app
+    assert "function TerritoryMapBridge" in app
     assert "setInterval(sync, 500)" in app
     assert "document.title = title" in app
 
